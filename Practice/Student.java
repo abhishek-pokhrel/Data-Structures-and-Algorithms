3ec772c961da15f108add83b1eaae23a47484704
abhishek-pokhrel <abhishekpokhrel53@gmail.com>
2024-09-12 22:54:53 -0400

    practice

---
+++ Practice/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    private String name;
+    private int rollNumber;
+    private char grade;
+
+    public Student(String name, int rollNumber, char grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getters and setters for the other attributes (omitted for brevity)
+}
